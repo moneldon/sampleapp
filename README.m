d5c04a68f04085801a653306e14dd653205bd845
moneldon <moneldon@gmail.com>
2012-03-02 00:13:36 +0530

    well done

--- README.m
+++ README.m
@@ -1,4 +1,5 @@
 # welcome to ruby
 
 
-this is sample app for [*Ruby on rails:learn rails*](http://google.com) by [*monel*] (http://facebook.com)
+this is sample app for
+ [Ruby on rails:learn rails](http://google.com/) by [monel] (http://facebook.com)
